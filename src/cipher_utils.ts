861eda88cb773a6a259e8c692c614c2c8d23dfbd
Michał Ziober <michalziober08@gmail.com>
2024-10-22 01:28:46 +0200

    added pepper to password

--- src/cipher_utils.ts
+++ src/cipher_utils.ts
@@ -5,10 +5,12 @@ const encryptionIV = createHash('sha512')
     .digest('hex')
     .substring(0, 16)
 const encryption_method = 'aes-256-cbc';
+const pepper = "sdf2453443dw!fwf";
 
 function encrypt(dataToEncrypt:any, password:string) {
+    const passwordWithPepper = password + pepper;
     const key = createHash('sha512')
-        .update(password)
+        .update(passwordWithPepper)
         .digest('hex')
         .substring(0, 32)
     const cipher = createCipheriv(encryption_method, key, encryptionIV)
@@ -18,8 +20,9 @@ function encrypt(dataToEncrypt:any, password:string) {
 }
 
 function decrypt(dataToDecrypt:string, password:string) {
+    const passwordWithPepper = password + pepper;
     const key = createHash('sha512')
-        .update(password)
+        .update(passwordWithPepper)
         .digest('hex')
         .substring(0, 32)
     const buff = Buffer.from(dataToDecrypt, 'base64')
