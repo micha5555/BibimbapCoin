2a9c93d651f9c165c50c0c9fe0e0895312409f7d
Michał Ziober <michalziober08@gmail.com>
2024-10-22 01:12:44 +0200

    added function for encyption and decryption; added file with sample using of functions

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
 import express, { Express, Request, Response } from "express";
 
-import {createId, generateKeys, hashPassword} from "./key_utils";
+import {createId, generateKeys, hashPassword, encrypt, decrypt} from "./key_utils";
 
 const app = express();
 
 app.get("/", async (request: Request, response: Response): Promise<void> => {
-    // const { publicKey, privateKey } = generateKeys();
-    // const id = createId(privateKey, publicKey);
-    // response.send(publicKey + "\n" + privateKey);
 
-    const promiseHashed = hashPassword("password");
-    const hashed = await promiseHashed.then((value) => {return value});
-    promiseHashed.then((value) => {console.log(value)});
-    // hashed.then((value) => console.log(value));
-    // console.log(promiseHashed.then((value) => console.log(value)));
-    console.log(hashed);
-    response.send(hashed);
-    // response.send(hashed);
+    response.send("Hello World");
 });
 
 app.listen(10000, (): void => {
