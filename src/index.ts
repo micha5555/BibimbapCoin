116ad282a407f12bd1633040e3cac2d6f3bafb65
Jakub Maciejewski <jakubm55555@gmail.com>
2024-10-22 00:27:55 +0200

    Adds asking for port number on startup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-// import express, { Request, Response } from ;
-// import crypto from "crypto";
-
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
+import readline from "readline";
 
 const app = express();
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let port: number;
+
+rl.question("Enter port number: ", (portInput: string) => {
+    port = parseInt(portInput);
+    rl.close();
+
+    if(isNaN(port)) {
+        console.log("Given port number is not a number!");
+        process.exit(1);
+    }
+
+    app.listen(port, (): void => {
+        console.log("Started application on port %d", port);
+    });
+});
 
 app.get("/", (request: Request, response: Response): void => {
     response.send("Hello, world!");
@@ -19,9 +37,5 @@ app.get("/get-neighbors", (request: Request, response: Response): void => {
 
 app.get("/is-alive", (request: Request, response: Response): void => {
     response.status(200)
-        .send("NAME Alive") //TODO: Add name
+        .send(`Node on ${port} is alive`)
 })
-
-app.listen(10000, (): void => {
-    console.log("Started application on port %d", 10000);
-});
