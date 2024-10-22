8125e0be975f142a3203a158604c6244e295aae4
Michał Ziober <michalziober08@gmail.com>
2024-10-22 02:01:22 +0200

    changes reading from console

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import readline from "readline";
 import {Controller} from "./controller";
 import {Node} from "./node";
 
-import {createId, generateKeys, hashPassword } from "./key_utils";
+import {createId, generateKeys, hashPassword} from "./key_utils";
 import {encrypt, decrypt} from "./cipher_utils";
+import {resolve} from "node:dns/promises";
 
 const app = express();
 app.use(express.json());
@@ -17,20 +18,42 @@ const rl = readline.createInterface({
 let controller: Controller;
 let node: Node = new Node();
 
-rl.question("Enter port number: ", (portInput: string) => {
-    let port = handlePortInput(portInput);
+const question1 = () => {
+    return new Promise<void>((resolve, reject) => {
+            rl.question("Enter password: ", (password: string) => {
 
-    controller = new Controller(app, port, node);
+                resolve();
+            });
+        }
+    )
+}
 
-    controller.defineControllerMethods();
-    controller.startController();
-});
+const question2 = () => {
+    return new Promise<void>((resolve, reject) => {
+            rl.question("Enter port number: ", (portInput: string) => {
+                let port = handlePortInput(portInput);
+                controller = new Controller(app, port, node);
+
+                controller.defineControllerMethods();
+                controller.startController();
+            });
+        }
+    )
+}
+
+
+const main = async () => {
+    await question1();
+    await question2();
+}
+
+main();
 
 
 // private functions
 function handlePortInput(portInput: string) {
     let port = parseInt(portInput);
-    rl.close();
+
 
     if (isNaN(port)) {
         console.log("Given port number is not a number!");
