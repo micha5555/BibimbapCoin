021ab3810fed335ec821fb9bdd7cba9157ef1409
Jakub Maciejewski <jakubm55555@gmail.com>
2025-01-02 18:29:44 +0100

    added ignore for dist/test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    testPathIgnorePatterns: [
+        '/node_modules/', // Ignoruj folder node_modules
+        '/dist/test/'     // Ignoruj folder dist/test
+    ],
 };
